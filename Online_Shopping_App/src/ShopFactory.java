25235d2e0f5e5f429beaefb4a65e9c410e5a2b75
takne <takne@Nikita>
2024-01-17 09:45:22 +0530

    Final commit

---
+++ Online_Shopping_App/src/ShopFactory.java
@@ -0,0 +1,8 @@
+
+abstract class ShopFactory {
+	public abstract PrimeAcc getNewPrimeAcc(int accNo, String accNm,
+			float charges, boolean isPrime); //method that create instance of PrimeAcc
+	
+	public abstract NormalAcc getNewNormalAcc(int accNo, String accNm, 
+			float charges, float deliveryCharges);
+}
